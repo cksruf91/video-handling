0558df39e0c62dbbe5519c99c8818a957fe0d2c1
박찬결 <chan10@hanatour.com>
2024-12-12 13:25:45 +0900

    update

--- core/extract_keyword.py
+++ core/extract_keyword.py
@@ -2,27 +2,19 @@ import json
 from pathlib import Path
 
 from client.openai.chat import OpenAIClient
+from core.prompt_manager import PromptManager
 from utile.progress_bar import ProgressBar
 
 
 class KeywordExtractor:
+    _PROMPT = PromptManager()
+
     def __init__(self, output_file: Path):
         print('Extracting keywords...')
         self.output_file = output_file
         self.data = json.load(self.output_file.open('r'))
         self.open_ai = OpenAIClient()
         print(f'\tL output file : {self.output_file}')
-
-        self.system_prompt = """
-        당신은 언어 전문가 입니다.
-        """
-        self.user_prompt = """
-        아래의 사항을 고려하여 중심이 되는 keyword를 추출해줘
-        - keyword 는 명사를 위주로 추출
-        - 콤마(",") 구분자로 하여 리스트 형태로 추출 ex) "keyword1,keyword2,keyword3..."
-
-        {full_text}
-        """
 
     def run(self) -> None:
         caption = self.data.get('caption')
@@ -31,7 +23,7 @@ class KeywordExtractor:
                 continue
             full_text = row['desc'] + ' ' + row['text']
             self.open_ai \
-                .add_prompt(role='system', text=self.system_prompt) \
-                .add_prompt(role='user', text=self.user_prompt.format(full_text=full_text))
+                .add_prompt(role='system', text=self._PROMPT.KEYWORD_SYSTEM) \
+                .add_prompt(role='user', text=self._PROMPT.KEYWORD_USER.format(full_text=full_text))
             row['keyword'] = self.open_ai.call()
         json.dump(self.data, self.output_file.open('w'), ensure_ascii=False, indent=2)
--- core/prompt_manager.py
+++ core/prompt_manager.py
@@ -7,5 +7,7 @@ class PromptManager:
     _FILE = Path('.').joinpath('core').joinpath('resource').joinpath('prompt.yaml')
     _PROMPT = yaml.safe_load(_FILE.open('r'))
 
-    SUMMARY = _PROMPT.get('app').get('summary').get('prompt')
-    CAPTIONING = _PROMPT.get('app').get('captioning').get('prompt')
+    SUMMARY = _PROMPT.get('app').get('summary').get('v1').get('prompt')
+    CAPTIONING = _PROMPT.get('app').get('captioning').get('v1').get('prompt')
+    KEYWORD_SYSTEM = _PROMPT.get('app').get('keyword').get('v1').get('system_prompt')
+    KEYWORD_USER = _PROMPT.get('app').get('keyword').get('v1').get('user_prompt')
